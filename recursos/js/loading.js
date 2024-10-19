bdd0d6d827c93e604ce1a38dac10076c7d1c999a
Cesar 4K <cesareduardoriveranajera@gmail.com>
2024-10-18 21:55:05 -0600

    union de segmentos web

---
+++ recursos/js/loading.js
@@ -0,0 +1,7 @@
+   /* PRELOADER SCREEN */
+   window.addEventListener('load', () => {
+    document.querySelector('.load').classList.add('load--hidden');
+});   /* PRELOADER SCREEN */
+window.addEventListener('load', () => {
+    document.querySelector('.load').classList.add('load--hidden');
+});
