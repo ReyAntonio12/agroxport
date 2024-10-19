ff4f2438c0f74bf4dc3c9ccde856d88fd50b2436
Reynaldo Castano <reycastano97@gmail.com>
2024-10-19 02:11:54 -0600

    Añadiendo exito.html para hacer saber al usuario que sus datos fueron recibidos y su cuenta sera creada

--- recursos/js/server.js
+++ recursos/js/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 
+//Acceder a archivos desde una carpeta estatica 
+app.use(express.static('public'));
 
 //Middleware para la base de datos
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +13,8 @@ app.use(express.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'AntonioRey121297*',
+    user: 'agrolink_user',
+    password: 'AgroLink2024!',
     database: 'agrolink_database'
   });
 
@@ -29,6 +31,11 @@ const connection = mysql.createConnection({
 app.post('/guardar-datos', (req, res) => {
     const { nom, tel, corr, ced, pw } = req.body;
 
+  // Validar que los campos requeridos no estén vacíos
+    if (!nom || !tel || !corr || !ced || !pw) {
+      return res.status(400).send('Todos los campos requeridos deben estar completos.');
+    }
+
  // Consulta SQL para insertar datos
  const sql = 'INSERT INTO socio (codigo_socio, nombre, direccion, telefono, email, cedula, nombre_de_empresa, ruc, codigo_minsa, contraseNa) VALUES (NULL, ?, NULL, ?, ?, ?, NULL, NULL, NULL, ?)';
  
@@ -39,7 +46,7 @@ app.post('/guardar-datos', (req, res) => {
      return res.status(500).send('Error al guardar datos');
    }
    console.log('Datos insertados:', result);
-   res.send('Datos guardados correctamente');
+   res.sendFile(__dirname + '/exito.html'); // Redirigiendo pagina a Exito.HTML
  });
 });
 
